0ea6194468bf83c314acd1214a3fb76af45851dd
OnlyDropped <asheburenkov@yandex.ru>
2021-12-16 22:04:48 +0300

    added burger menu

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@
     $(".header-nav__form, .header-nav__input").toggleClass("active");
     $("input[type='search']").focus();
   });
+
+
+  $('.header-nav__btn').click(function () {
+    $(".header-nav__list").toggleClass("active");
+  });
